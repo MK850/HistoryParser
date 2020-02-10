521252a50593b220fc4a78d7b7a297ee46f99440
MK850 <bom8231@naver.com>
2020-02-11 06:01:46 +0900

    Like this?

--- HistoryParser.py
+++ HistoryParser.py
@@ -14,22 +14,6 @@ basic=2 #한 바이트 읽어오기 위한 단위
 byte_4=basic*4 #4바이트 읽어오기
 byte_8=basic*8 #8바이트 읽어오기
 
-#헥스를 텍스트로 읽어오는 함수
-def READ(URL_text):
- length=len(URL_text)
- Slice=[]
- for i in range(0,length,2):
-    Slice.append(URL_text[i:i+2])
- charTohex=[]
- for i in range(0,len(Slice)):
-    charTohex.append(int(Slice[i],16))
- charToAscii=[]
- for i in range(0,len(Slice)):
-    charToAscii.append(chr(charTohex[i]))
- for i in range(0,len(charToAscii)):
-    print(charToAscii[i],end="")
- print()
-
 #URL 리스트 함수
 def URL_LIST(URL_text):
  lengh=len(URL_text)
@@ -45,40 +29,13 @@ def URL_LIST(URL_text):
  URL_list = "".join(charToAscii)
  return URL_list
 
-#레코드 크기 변환 함수
-def Record_Size(size_str):
-    size_int=int(size_str)
-    Size=128*size_int
-    print("Record size:",Size)
-
 #레코드 크기 리스트 함수
 def Record_Size_list(size_str):
     size_int=int(size_str)
     Size=128*size_int
     return (str(Size)+"byte")
 
-#헥스값 -> 년도날짜시간 변환 함수
-def Time(time):
-    Time_Big = time
-    BigToLittle = []  #빅엔디안->리틀엔디안
-    for i in range(0, len(Time_Big), 2):
-        BigToLittle.append(Time_Big[i:i + 2])
-    BigToLittle.reverse()
-    Time_big = "".join(BigToLittle)
-
-    hex_Time = int(Time_big, 16)
-    ns_100 = 10000000  # 100나노초 값
-    Second = hex_Time / ns_100
-
-    # datetime 모듈이용해 초를 기존 시간에 더하기
-    stand_time = datetime(1601, 1, 1, 00, 00, 00)
-    Second_After = stand_time + timedelta(seconds=Second)
-
-    UTC_9 = Second_After + timedelta(hours=9)
-    Trimed_Time = UTC_9.strftime('%Y-%m-%d %H:%M:%S')
-    print(Trimed_Time, "(+09:00)")
-
-#시간 리스트 만들기 위함
+#시간 리스트
 def Time_List(time):
     Time_Big = time
     BigToLittle = []  #빅엔디안->리틀엔디안
@@ -99,17 +56,6 @@ def Time_List(time):
     Trimed_Time = UTC_9.strftime('%Y-%m-%d %H:%M:%S')
     return  (str(Trimed_Time)+"(+09:00)")
 
-#방문 횟수 함수
-def Visit(visit_count):
-    Big_visit= visit_count
-    visitBigtoLittle = []  #빅엔디안->리틀엔디안
-    for i in range(0, len(Big_visit), 2):
-        visitBigtoLittle.append(Big_visit[i:i + 2])
-    visitBigtoLittle.reverse()
-    Time_big = "".join(visitBigtoLittle)
-    hex_Visit_count= int(Time_big, 16)
-    print("Visit count:",hex_Visit_count)
-
 #방문 횟수 리스트 함수
 def Visit_list(visit_count):
     Big_visit= visit_count
@@ -125,7 +71,6 @@ def Visit_list(visit_count):
 def Web_list(urls):
 
     visit_CoUnt=visit_Count_list
-
     web_sig_url = []
 
     for i in range(len(urls)):
@@ -169,6 +114,7 @@ URL_List=[]
 Record_Size_List=[]
 Last_fixTime_List=[]
 Last_visitTime_List=[]
+IE_Hisory=[]
 
 for i in range(len(Start_sign)):
     URL_sign=Start_sign[i] #URL 시그니쳐 검색
@@ -177,7 +123,6 @@ for i in range(len(Start_sign)):
     last_fix=hex[URL_sign+2*byte_4:URL_sign+2*byte_8]
     last_visited=hex[URL_sign+2*byte_8:URL_sign+3*byte_8]
     visit_count=hex[URL_sign+168:URL_sign +168+byte_4]
-    url=hex[URL_sign+2*byte_8+176:END_URL]
     url_trimed = hex[URL_sign + 2 * byte_8 + 208:END_URL]
 
     visit_Count_list.append(Visit_list(visit_count))
@@ -187,19 +132,13 @@ for i in range(len(Start_sign)):
     Last_visitTime_List.append(Time_List(last_visited))
 
     if __name__ == "__main__":
+        IE_Hisory.append(URL_List[i])
+        IE_Hisory.append(Record_Size_List[i])
+        IE_Hisory.append(visit_Count_list[i])
+        IE_Hisory.append(Last_fixTime_List[i])
+        IE_Hisory.append(Last_visitTime_List[i])
 
-        # 히스토리 출력
-
-        print()
-
-        print('[', i + 1, '] ', end="")
-        READ(url)
-        Record_Size(size_str)
-        Visit(visit_count)
-        print("Last fixed time: ", end="")
-        Time(last_fix)
-        print("Last visited time: ", end="")
-        Time(last_visited)
+        print(IE_Hisory)
 
 Counter=(Web_list(URL_List))
 web_key = list(Counter.keys())
